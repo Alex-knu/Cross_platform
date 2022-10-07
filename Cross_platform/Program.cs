a05d3cb929f4a6324294385b755a3cb0880df8f3
Alex Rozdobudko <arozdobudko2@gmail.com>
2022-10-07 10:53:44 +0300

    Add project files.

---
+++ Cross_platform/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cross_platform
+{
+
+    class Program
+    {
+        static int Algoritm(int items)
+        {
+            return items * (items + 2) * (2 * items + 1) / 8;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Algoritm(4));
+        }
+    }
+}
