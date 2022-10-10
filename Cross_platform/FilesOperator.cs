09d12927234583f03582e5fedf21439b2701ef5b
Alex Rozdobudko <arozdobudko2@gmail.com>
2022-10-10 09:34:40 +0300

    Completed laboratory

---
+++ Cross_platform/FilesOperator.cs
@@ -0,0 +1,40 @@
+﻿namespace Cross_platform
+{
+    public static class FilesOperator
+    {
+        public static int? ReadData(string path)
+        {
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+                return Int32.Parse(lines[0]);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Exception: incorrect data in file");
+            }
+
+            return null;
+        }
+
+        public static void WriteData(string path, string data)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine(data);
+                sw.Close();
+
+                Console.WriteLine("See result on '{0}': ", path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+    }
+}
--- Cross_platform/Program.cs
+++ Cross_platform/Program.cs
@@ -5,12 +5,37 @@
     {
         static int Algoritm(int items)
         {
-            return items * (items + 2) * (2 * items + 1) / 8;
+            return (int)(items * (items + 2) * (2 * items + 1) / 8);
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Algoritm(4));
+            int? data = null;
+            Console.WriteLine("Input path for input data file");
+            string? inputPath = Console.ReadLine();
+            Console.WriteLine("Input path for output data file");
+            string? outputPath = Console.ReadLine();
+
+            if (inputPath == null || outputPath == null)
+            {
+                Console.WriteLine("Exception: emty path to input/output file");
+                return;
+            }
+
+            data = FilesOperator.ReadData(inputPath);
+
+            if (!data.HasValue)
+            {
+                return;
+            }
+
+            if (data <= 0)
+            {
+                Console.WriteLine("Number must be more than zero");
+                return;
+            }
+
+            FilesOperator.WriteData(outputPath, (Algoritm(data.Value)).ToString());
         }
     }
 }
